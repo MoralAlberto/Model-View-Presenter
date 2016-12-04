0dd776308937afa711c03521f3ee35632a5b1ac8
Alberto Moral <alberto.moral.g@gmail.com>
2016-12-04 18:35:56 +0100

    Add protocol for data provider

---
+++ Presenter/UI/View/DataSource/Protocols/DataProviderPresenterProtocol.swift
@@ -0,0 +1,9 @@
+
+
+protocol DataProviderPresenterProtocol {
+    associatedtype View
+    associatedtype Object
+    func objectAtIndexPath(_ index: Int) -> Object
+    func configure(item: View, at index: Int)
+    func numberOfItemsInSection(_ section: Int) -> Int
+}
